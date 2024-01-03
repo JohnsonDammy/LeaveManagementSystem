9f3cb97b530ac45ba3ef0eb7a12dd37933f7695a
user <user@DCODER>
2024-01-03 15:24:11 +0200

    Add project files.

---
+++ Leave/Controllers/AdminController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Leave.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult admin_dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult add_staff()
+        {
+            return View();
+        }
+
+
+        public ActionResult apply_leave()
+        {
+            return View();
+        }
+
+
+        public ActionResult approved_leave()
+        {
+            return View();
+        }
+
+
+        public ActionResult departnent()
+        {
+            return View();
+        }
+
+
+        public ActionResult edit_department()
+        {
+            return View();
+        }
+
+
+        public ActionResult edit_leave_type()
+        {
+            return View();
+        }
+
+
+        public ActionResult edit_staff()
+        {
+            return View();
+        }
+
+
+        public ActionResult index()
+        {
+            return View();
+        }
+
+        public ActionResult leave_details()
+        {
+            return View();
+        }
+
+        public ActionResult leave_type()
+        {
+            return View();
+        }
+
+        public ActionResult leaves()
+        {
+            return View();
+        }
+
+        public ActionResult my_profile()
+        {
+            return View();
+        }
+
+        public ActionResult pending_leave()
+        {
+            return View();
+        }
+
+        public ActionResult rejected_leave()
+        {
+            return View();
+        }
+
+        public ActionResult staff()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
---
+++ Leave/Controllers/StaffController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Leave.Controllers
+{
+    public class StaffController : Controller
+    {
+        // GET: Staff
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult apply_leave()
+        {
+            return View();
+        }
+
+
+        public ActionResult leave_history()
+        {
+            return View();
+        }
+
+        public ActionResult staff_profile()
+        {
+            return View();
+        }
+
+        public ActionResult view_leave()
+        {
+            return View();
+        }
+    }
+}
